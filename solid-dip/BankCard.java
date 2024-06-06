a060065d4fdede5f0f9910ca3d8df1741fce1961
Saloni Punjabi <82083834+salonipunjabi4@users.noreply.github.com>
2024-06-06 14:49:44 -0400

    Add files via upload

---
+++ solid-dip/BankCard.java
@@ -0,0 +1,8 @@
+package com.demo.dip;
+
+public interface BankCard {
+	public void doTransaction();
+	
+	
+
+}
---
+++ solid-dip/ShoppingMall.java
@@ -0,0 +1,15 @@
+package com.demo.dip;
+
+public class ShoppingMall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BankCard bc = new ADebitCard();
+		if(bc instanceof DebitCard) {
+			
+		}
+
+	}
+
+}
